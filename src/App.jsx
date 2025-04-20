9d2263db030600dabd1e044dd50012637e3a318b
emonShimoul <emonshimoul@gmail.com>
2025-04-21 01:18:03 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="font-bold text-2xl mb-4">Vite + React</h1>
+      <span className="loading loading-spinner text-primary"></span>
+      <span className="loading loading-spinner text-secondary"></span>
+      <span className="loading loading-spinner text-accent"></span>
+      <span className="loading loading-spinner text-neutral"></span>
+      <span className="loading loading-spinner text-info"></span>
+      <span className="loading loading-spinner text-success"></span>
+      <span className="loading loading-spinner text-warning"></span>
+      <span className="loading loading-spinner text-error"></span>
+    </div>
+  );
+}
+
+export default App;
