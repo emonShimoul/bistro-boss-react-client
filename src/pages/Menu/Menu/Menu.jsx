212aac6e511ddaa055b0477cdcf989d37e8cf79b
emonShimoul <emonshimoul@gmail.com>
2025-04-21 10:26:29 +0600

    Display food items on different tabs

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -33,15 +33,15 @@ const Menu = () => {
       {/* dessert menu items */}
       <MenuCategory
         items={dessert}
-        title="Dessert"
+        title="dessert"
         img={dessertImg}
       ></MenuCategory>
       {/* pizza menu items */}
-      <MenuCategory items={pizza} title="Pizza" img={pizzaImg}></MenuCategory>
+      <MenuCategory items={pizza} title="pizza" img={pizzaImg}></MenuCategory>
       {/* salad menu items */}
-      <MenuCategory items={salad} title="Salad" img={saladImg}></MenuCategory>
+      <MenuCategory items={salad} title="salad" img={saladImg}></MenuCategory>
       {/* soup menu items */}
-      <MenuCategory items={soup} title="Soup" img={soupImg}></MenuCategory>
+      <MenuCategory items={soup} title="soup" img={soupImg}></MenuCategory>
     </div>
   );
 };
