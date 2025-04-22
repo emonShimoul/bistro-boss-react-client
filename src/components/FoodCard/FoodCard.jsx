3f21c294714f258f2b870d9631cf9a4a45de4549
emonShimoul <emonshimoul@gmail.com>
2025-04-22 08:11:33 +0600

    Setup Recaptcha And Validate Captcha

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -9,11 +9,13 @@ const FoodCard = ({ item }) => {
         </p>
         <img src={image} alt={name} />
       </figure>
-      <div className="card-body">
+      <div className="card-body flex flex-col items-center">
         <h2 className="card-title">{name}</h2>
         <p>{recipe}</p>
         <div className="card-actions justify-end">
-          <button className="btn btn-primary">Add to Cart</button>
+          <button className="btn btn-outline border-0 border-b-4 mt-4 bg-slate-100 border-orange-400">
+            Add to Cart
+          </button>
         </div>
       </div>
     </div>
