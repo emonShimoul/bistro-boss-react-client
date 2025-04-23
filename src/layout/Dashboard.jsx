b5dbaeb20f13cf7fc517baab19e700fbe6e47c52
emonShimoul <emonshimoul@gmail.com>
2025-04-23 09:13:31 +0600

    Cart data from custom hook and calculate total price

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -3,19 +3,22 @@ import {
   FaCalendar,
   FaHome,
   FaList,
+  FaSearch,
   FaShoppingCart,
 } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+  const [cart] = useCart();
   return (
-    <div className="flex gap-8">
+    <div className="flex">
       {/* dashboard sidebar */}
       <div className="w-64 min-h-screen bg-orange-400">
         <ul className="menu p-4">
           <li>
             <NavLink to="/dashboard/cart">
-              <FaShoppingCart></FaShoppingCart>My Cart
+              <FaShoppingCart></FaShoppingCart>My Cart ({cart.length})
             </NavLink>
           </li>
           <li>
@@ -38,10 +41,21 @@ const Dashboard = () => {
               <FaHome></FaHome> User Home
             </NavLink>
           </li>
+          <div className="divider"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome></FaHome> Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/order/salad">
+              <FaSearch></FaSearch> Order
+            </NavLink>
+          </li>
         </ul>
       </div>
       {/* dashboard content */}
-      <div className="flex-1 mt-4">
+      <div className="flex-1 p-10">
         <Outlet></Outlet>
       </div>
     </div>
